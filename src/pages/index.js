b3c8ca1813432b1377ce02d5461e2857efbb66af
Hugh Zhang <hugh@Hughs-MacBook-Pro-15.local>
2018-10-23 03:36:27 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to React</h1>
+    <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
